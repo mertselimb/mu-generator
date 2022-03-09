69a57c4aba21396488e9ed3186a6ea617635d90e
Mert Selimbeyoğlu <mertselimb@gmail.com>
2022-03-09 21:51:41 +0300

    change isEnd to end

--- src/app/answers/answers.component.ts
+++ src/app/answers/answers.component.ts
@@ -12,7 +12,7 @@ export class AnswersComponent implements OnInit {
   ngOnInit(): void {}
 
   createAnswer(): void {
-    this.answers.push({ text: '', outcome: {}, isEnd: false });
+    this.answers.push({ text: '', outcome: {}, end: false });
   }
 
   createNextDialog(answer: Answer): void {
--- src/app/dataModels/answer.ts
+++ src/app/dataModels/answer.ts
@@ -4,6 +4,6 @@ import { Stats } from './outcome';
 export interface Answer {
   text: string;
   outcome: Stats;
-  isEnd: Boolean;
+  end: Boolean;
   nextDialogs?: Dialog[];
 }
